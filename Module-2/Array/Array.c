474a492852dddbc5af8d5ea96c656f0d6494f0e9
tops-nidhi <109589571+tops-nidhi@users.noreply.github.com>
2023-05-08 11:36:18 +0530

    add

---
+++ Module-2/Array/Array.c
@@ -0,0 +1,8 @@
+// Array use to store the multiple value in one variable
+#include<stdio.h>
+int main()
+{
+    int array[5] = {101,505,606,202,34};
+    printf("\nArray[3]:%d",array[3]);
+    return 0;
+}
