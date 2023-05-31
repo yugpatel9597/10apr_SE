9d2c4861bf09beb1627a5df52c45adec0b22ee1c
tops-nidhi <109589571+tops-nidhi@users.noreply.github.com>
2023-05-31 12:14:09 +0530

    add

---
+++ Module-3/Basic/Getdata.cpp
@@ -0,0 +1,14 @@
+// WAP to get the student id and name from the user.
+#include<iostream>
+using namespace std;
+int main()
+{
+    int id;
+    string nm;
+    cout<<"Enter student id : ";
+    cin>>id;
+    cout<<"Enter student name : ";
+    cin>>nm;
+    cout<<nm<<" your id is: "<<id;
+    return 0;
+}
---
+++ Module-3/Basic/hello.cpp
@@ -0,0 +1,7 @@
+// WAP to print hello world in c++
+#include<iostream>
+int main()
+{
+    std::cout<<"Hello World";
+    return 0;
+}
